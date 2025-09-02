cdcde07a1a0818fea9ec721988d118dbf72bb18f
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-02 04:05:29 +0200

    Add project files.

---
+++ TestHelper/HelperMethods.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace TestHelper
+{
+    public static class HelperMethods
+    {
+        public static void AssertOutputs(string assemblyName, string expected)
+        {
+            var sw = new StringWriter();
+            var originalOut = Console.Out;
+            Console.SetOut(sw);
+
+            try
+            {
+                // Act: invoke the top-level statements
+                // Top-level statements in .NET 6+ generate a hidden Program.Main method.
+                var assembly = Assembly.Load(assemblyName.Substring(4)); // replace with your assembly name
+                var entryPoint = assembly.EntryPoint;
+                entryPoint.Invoke(null, entryPoint.GetParameters().Length == 0 ? null : new object[] { Array.Empty<string>() });
+
+                // Assert
+                var output = sw.ToString();
+                StringAssert.Contains(output, expected);
+            }
+            finally
+            {
+                Console.SetOut(originalOut);
+            }
+        }
+    }
+}
---
+++ TestOefening1A/Test1.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace TestOefening1A
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestHelloWorld()
+        {
+            TestHelper.HelperMethods.AssertOutputs(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name, "Hello world!");
+        }
+    }
+}
