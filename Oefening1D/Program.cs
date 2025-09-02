fb53831542c87855e1da5ed5b1ebf2e0639a86f0
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-02 11:52:38 +0200

    1C opgave

--- Oefening1D/Program.cs
+++ Oefening1D/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// d.	Kies een favoriete quote van een film, steek die in een variabele (quote) en schrijf op het scherm: Mijn favoriete quote: "qoute"
