b51a4c00691297483504cf0644b806e7de32fbb3
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-04 15:46:46 +0200

    data oef 2

---
+++ Herhaling examen 2 5ICW - 2024-2025 - Oefening 2/Personage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Games.Klassen
+{
+    public class Personage
+    {
+        public string Naam { get; set; }
+        public string Rol { get; set; } 
+        public string Beschrijving { get; set; }
+        public int Leeftijd { get; set; } 
+        public string SpecialeKracht { get; set; }
+        public bool IsSpeelbaar { get; set; }
+
+        public Personage(string naam, string rol, string beschrijving, int leeftijd, string specialeKracht, bool isSpeelbaar)
+        {
+            Naam = naam;
+            Rol = rol;
+            Beschrijving = beschrijving;
+            Leeftijd = leeftijd;
+            SpecialeKracht = specialeKracht;
+            IsSpeelbaar = isSpeelbaar;
+        }
+        public override string? ToString()
+        {
+            return $"{Naam} ({Rol})";
+        }
+    }
+
+}
