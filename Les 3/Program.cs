7fe1c901b51f1f6bb5926688af74d715c2497955
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-05 11:03:59 +0200

    les 3

---
+++ Les 3/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] nummers = new int[3];
+
+nummers[0] = 1;
+nummers[1] = 2;
+nummers[2] = 3;
+
+
+List<int> list = new List<int>();
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(1);
+list.Remove(1);
+
+foreach(var i in nummers)
+{
+    Console.WriteLine(i);
+}
+
+for(var i = 0; i < 3; i++)
+{
+    Console.WriteLine(nummers[i]);
+}
