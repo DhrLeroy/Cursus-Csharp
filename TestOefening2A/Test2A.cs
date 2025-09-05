567d970f9be23baca90fb98a8be85d4740928452
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-05 05:54:09 +0200

    Test2A

--- TestOefening2A/Test2A.cs
+++ TestOefening2A/Test2A.cs
@@ -8,5 +8,10 @@
         {
             TestHelper.HelperMethods.AssertOutputs("Geef a in: Geef b in: Som: 3 + 2 = 5\r\nVerschil: 3 - 2 = 1\r\nProduct: 3 x 2 = 6\r\nQuotiënt: 3 / 2 = 1,5", "3", "2");//
         }
+        [TestMethod]
+        public void Test41()
+        {
+            TestHelper.HelperMethods.AssertOutputs("Geef a in: Geef b in: Som: 4 + 1 = 5\r\nVerschil: 4 - 1 = 3\r\nProduct: 4 x 1 = 4\r\nQuotiënt: 4 / 1 = 4", "4", "1");//
+        }
     }
 }
