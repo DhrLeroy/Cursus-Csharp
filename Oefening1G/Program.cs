5b00df6aa3b4ed7398d8a0517c950365212b7fed
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-05 08:49:35 +0200

    1G opgave

--- Oefening1G/Program.cs
+++ Oefening1G/Program.cs
@@ -1,1 +1,1 @@
-﻿// g.	Vraag aan de gebruikers eerst diens naam, daarna diens favoriete film en toon het volgende op het scherm: Hallo naam, ik kijk ook graag naar film.
+﻿// g.	Vraag aan de gebruikers eerst diens naam ("Naam: "), daarna diens favoriete film ("Favoriete film: ") en toon het volgende op het scherm: Hallo naam, ik kijk ook graag naar film.
