98ba583525756d60c2197bf9517cd59a6e1417a1
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-09 11:37:18 +0200

    fix 4D

--- Oefening 4D/Program.cs
+++ Oefening 4D/Program.cs
@@ -8,16 +8,4 @@ Gemaskeerd: H***O
  */
 
 
-Console.Write("Woord: ");
-
-string woord = Console.ReadLine();
-
-Console.Write("Gemaskeerd: ");
-
-string sterretjes = "";
-for(int i = 0; i < woord.Length-2; i++)
-{
-    sterretjes += "*";
-}
-
-Console.Write($"{woord.Substring(0, 1).ToUpper()}{sterretjes}{woord.Substring(woord.Length - 1, 1).ToUpper()}");
+Console.WriteLine();
--- Oplossing 4D/Program.cs
+++ Oplossing 4D/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Woord: ");
+
+string woord = Console.ReadLine();
+
+Console.Write("Gemaskeerd: ");
+
+string sterretjes = "";
+for (int i = 0; i < woord.Length - 2; i++)
+{
+    sterretjes += "*";
+}
+
+Console.Write($"{woord.Substring(0, 1).ToUpper()}{sterretjes}{woord.Substring(woord.Length - 1, 1).ToUpper()}");
