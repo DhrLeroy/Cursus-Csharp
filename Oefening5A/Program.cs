dd3f77a6bcc1a1812c32fba551519439e6d996cf
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-09 16:10:17 +0200

    5A fix

--- Oefening5A/Program.cs
+++ Oefening5A/Program.cs
@@ -7,24 +7,5 @@ Als alles goed is, toon je: "E-mailadres gevalideerd".
 Zolang het e-mailadres niet is gevalideerd, vraag je dit opnieuw.
  */
 
-bool gevalideerd = false;
 
-do
-{
-    gevalideerd = true;
-    Console.Write("E-mailadres: ");
-    string email = Console.ReadLine();
-    if (!email.Contains("@"))
-    {
-        Console.WriteLine("Geen @-teken in het e-mailadres");
-        gevalideerd = false;
-    }
-    if(email.LastIndexOf(".") > email.Length - 4)
-    {
-        Console.WriteLine("E-mailadres heeft geen geldig einde");
-        gevalideerd = false;
-    }
-} while (!gevalideerd);
-
-Console.WriteLine("E-mailadres gevalideerd");
 
