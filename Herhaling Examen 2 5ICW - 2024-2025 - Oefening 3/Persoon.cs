abfaecb2f28a4f56159f509d6a99d37aa098b5ca
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-10 09:52:08 +0200

    ex oef 3

---
+++ Herhaling Examen 2 5ICW - 2024-2025 - Oefening 3/Persoon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herhaling_Examen_2_5ICW___2024_2025___Oefening_3
+{
+    public class Persoon
+    {
+        public string Voornaam { get; set; }
+        public string Naam { get; set; }
+        public DateTime Geboortedatum { get; set; }
+
+        public Persoon(string voornaam, string naam, int geboortejaar, int geboortemaand, int geboorteday)
+        {
+            Voornaam = voornaam;
+            Naam = naam;
+            Geboortedatum = new DateTime(geboortejaar, geboortemaand, geboorteday);
+        }
+
+        public int BerekenLeeftijd()
+        {
+            int leeftijd = DateTime.Today.Year - Geboortedatum.Year;
+            DateTime verjaardagDitJaar = Geboortedatum.AddYears(leeftijd);
+            if (verjaardagDitJaar > DateTime.Today)
+                leeftijd--;
+            return leeftijd;
+        }
+    }
+}
--- Herhaling Examen 2 5ICW - 2024-2025 - Oefening 3/Program.cs
+++ Herhaling Examen 2 5ICW - 2024-2025 - Oefening 3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Herhaling_Examen_2_5ICW___2024_2025___Oefening_3;
+
+var personen = new List<Persoon>()
+{
+    new Persoon("Max", "Verstappen", 1997, 9, 30),
+    new Persoon("Andrea Kimi", "Antonelli", 2006, 8, 25),
+    new Persoon("Lando", "Norris", 1999, 11, 13),
+    new Persoon("Jean-Marie", "Jamal", 2000, 2, 29)
+};
+
+
+foreach(var persoon in personen)
+{
+    Console.WriteLine($"{persoon.Voornaam} {persoon.Naam} is {persoon.BerekenLeeftijd()} jaar.");
+}
