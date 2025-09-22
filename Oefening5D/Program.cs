11688161d435a5bcd55ddaf200d32c78482cb656
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-22 14:59:55 +0200

    5D

---
+++ Oefening5D/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Maak een programma dat alle priemgetallen bepaald tussen 2 getallen.
+ 
+Bv.
+Geef een ondergrens: 100
+Geef een bovengrens: 110
+Priemgetallen tussen 100-110 zijn: 101, 103, 107, 109
+ 
+ */ 
