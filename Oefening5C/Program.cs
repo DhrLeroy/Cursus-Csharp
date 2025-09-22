70de6a08335ef89083f8b5bb13a6f8ddc5e959c5
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-22 14:58:05 +0200

    oef5C

---
+++ Oefening5C/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Maak een programma dat bepaald of een getal een priemgetal is ("Geef een getal: ").
+ 
+Bv.
+Geef een getal: 4
+4 is geen priemgetal.
+
+Geef een getal: 5
+5 is een priemgetal.
+ 
+ */ 
