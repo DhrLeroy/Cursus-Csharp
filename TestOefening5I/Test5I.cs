384de1ab622854ddcff8b2c7417849678acf937a
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-23 16:07:54 +0200

    Test5I

---
+++ TestOefening5I/Test5I.cs
@@ -0,0 +1,17 @@
+﻿namespace TestOefening5I
+{
+    [TestClass]
+    public sealed class Test5I
+    {
+        [TestMethod]
+        public void Test18()
+        {
+            TestHelper.HelperMethods.AssertOutputs("Getal: 2 is een deler.\r\n3 is een deler.\r\n3 is een deler.\r\n18 = 2 x 3 x 3.", "18");
+        }
+        [TestMethod]
+        public void Test4()
+        {
+            TestHelper.HelperMethods.AssertOutputs("Getal: 2 is een deler.\r\n2 is een deler.\r\n4 = 2 x 2.", "4");
+        }
+    }
+}
