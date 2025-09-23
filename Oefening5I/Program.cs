e4a35375dbde76e394f766dd302115b8df45fb8e
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-23 16:04:00 +0200

    5I

---
+++ Oefening5I/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Hoofdstelling van de rekenkunde
+Elk natuurlijk getal groter dan 1 kan op een unieke manier geschreven worden als het product van priemgetallen. (op volgorde na)
+
+Gevraagd:
+Schrijf een programma dat een natuurlijk getal groter dan 1 vraagt en vervolgens alle priemfactoren afdrukt. 
+De priemgetallen worden van klein naar groot op het scherm weergegeven.
+
+Voorbeeld
+Getal: 18
+
+2 is een deler.
+3 is een deler.
+3 is een deler.
+18 = 2 x 3 x 3.
+ */
