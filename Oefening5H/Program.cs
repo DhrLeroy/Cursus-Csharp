a63a092b1b96e7cb64b642d261a3c0395c07a278
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-23 15:50:32 +0200

    Oef 5H

---
+++ Oefening5H/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ Samengestelde interest laat geld steeds meer aangroeien.
+
+Gevraagd
+Schrijf een programma dat vraagt naar het start- en eindbedrag en de jaarlijkse interest;
+Bereken en toon het eindbedrag en geef alle tussentijdse bedragen ook weer. Rond af op twee decimalen.
+Voorbeeld
+Startbedrag: 1520,25
+Eindbedrag: 2000
+Interest: 2,10%
+
+Na 1 jaar staat er € 1552.18 op de rekening.
+Na 2 jaar staat er € 1584.77 op de rekening.
+Na 3 jaar staat er € 1618.05 op de rekening.
+Na 4 jaar staat er € 1652.03 op de rekening.
+Na 5 jaar staat er € 1686.72 op de rekening.
+...
+Na 10 jaar staat er € 1871.43 op de rekening.
+Na 11 jaar staat er € 1910.72 op de rekening.
+Na 12 jaar staat er € 1950.85 op de rekening.
+Na 13 jaar staat er € 1991.82 op de rekening.
+Na 14 jaar staat er € 2033.65 op de rekening.
+ */
