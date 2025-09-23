b7b0315c3a3ede7ff2bd41b4d318b7baf4a7b3d6
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-23 03:51:52 +0200

    5B test

--- TestOefening5B/Test5B.cs
+++ TestOefening5B/Test5B.cs
@@ -11,14 +11,19 @@ namespace TestOefening5B
             HelperMethods.AssertOutputs("Beste optie: allebei hetzelfde voor 375 euro.", "30", "30");
         }
         [TestMethod]
-        public void Test15_4()
+        public void Test15_0()
         {
-            HelperMethods.AssertOutputs("Beste optie: allebei hetzelfde voor 375 euro.", "30", "30");
+            HelperMethods.AssertOutputs("Beste optie: Pizzeria Don Corleone voor 187,5 euro.", "15", "0");
         }
         [TestMethod]
-        public void Test80_3()
+        public void Test80_0()
         {
-            HelperMethods.AssertOutputs("Beste optie: allebei hetzelfde voor 375 euro.", "30", "30");
+            HelperMethods.AssertOutputs("Beste optie: Pizzeria Scaletta voor 832 euro.", "80", "0");
+        }
+        [TestMethod]
+        public void Test80_800()
+        {
+            HelperMethods.AssertOutputs("Beste optie: Pizzeria Don Corleone voor 1000 euro.", "80", "800");
         }
     }
 }
