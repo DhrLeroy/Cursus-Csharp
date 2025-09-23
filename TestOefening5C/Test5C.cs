ffbedcf4a38b1bea161ee60114f92b1e9e3582c1
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-23 04:01:13 +0200

    5C

---
+++ TestOefening5C/Test5C.cs
@@ -0,0 +1,37 @@
+﻿namespace TestOefening5C
+{
+    [TestClass]
+    public sealed class Test5C
+    {
+        [TestMethod]
+        public void sTest0()
+        {
+            TestHelper.HelperMethods.AssertOutputs("0 is geen priemgetal.", "0");
+        }
+        [TestMethod]
+        public void Test1()
+        {
+            TestHelper.HelperMethods.AssertOutputs("1 is geen priemgetal.", "1");
+        }
+        [TestMethod]
+        public void Test4()
+        {
+            TestHelper.HelperMethods.AssertOutputs("4 is geen priemgetal.", "4");
+        }
+        [TestMethod]
+        public void Test5()
+        {
+            TestHelper.HelperMethods.AssertOutputs("5 is een priemgetal.", "5");
+        }
+        [TestMethod]
+        public void Test40()
+        {
+            TestHelper.HelperMethods.AssertOutputs("40 is geen priemgetal.", "40");
+        }
+        [TestMethod]
+        public void Test101()
+        {
+            TestHelper.HelperMethods.AssertOutputs("101 is een priemgetal.", "101");
+        }
+    }
+}
