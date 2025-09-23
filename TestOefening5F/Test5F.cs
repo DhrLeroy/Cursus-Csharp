cdcdbc388f0a7962e75063757725d840705a5e76
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-23 08:45:38 +0200

    5F test

---
+++ TestOefening5F/Test5F.cs
@@ -0,0 +1,29 @@
+﻿using TestHelper;
+
+namespace TestOefening5F
+{
+    [TestClass]
+    public sealed class Test5F
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            HelperMethods.AssertOutputs("Leeftijd hond: Mensenjaren: 15", "1");
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            HelperMethods.AssertOutputs("Leeftijd hond: Mensenjaren: 24", "2");
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            HelperMethods.AssertOutputs("Leeftijd hond: Mensenjaren: 29", "3");
+        }
+        [TestMethod]
+        public void Test4()
+        {
+            HelperMethods.AssertOutputs("Leeftijd hond: Mensenjaren: 34", "4");
+        }
+    }
+}
