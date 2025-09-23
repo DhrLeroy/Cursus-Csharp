3d5e3e69b8bd61a5b6a644df6afa3d3de68cd2ca
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-23 08:42:29 +0200

    5F

---
+++ Oefening5F/Program.cs
@@ -0,0 +1,19 @@
+﻿/*  Schrijf een programma dat de leeftijd van de hond vraagt en 
+    vervolgens het equivalente aantal mensenjaren afdrukt.
+    Het eerste levensjaar van een hond telt meteen voor 15 mensenjaren;
+    het tweede levensjaar telt voor 9 extra mensenjaren;
+    elk volgend jaar telt voor 5 extra mensenjaren.
+ 
+Bv. Leeftijd hond: 1
+    Mensenjaren: 15
+
+    Leeftijd hond: 2
+    Mensenjaren: 24
+
+    Leeftijd hond: 3
+    Mensenjaren: 29
+
+    Leeftijd hond: 4
+    Mensenjaren: 34
+ */
+
