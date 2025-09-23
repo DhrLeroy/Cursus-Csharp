c98ebce2a4fbb9a8c59ac2a41d169046a306f0b5
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-23 15:44:42 +0200

    5G

---
+++ Oefening5G/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Indien drie lengtes gegeven worden kan je eenvoudig nagaan of 
+deze een driehoek vormen met behulp van de driehoeksongelijkheid.
+
+Driehoeksongelijkheid: in een driehoek is de som van twee zijden steeds groter dan de derde zijde.
+
+Opgave:
+Schrijf een programma dat de drie zijden van een driehoek vraagt en 
+nadien onderzoekt of deze al dan niet een driehoek bepalen.
+
+Voorbeelden
+Zijde 1: 4
+Zijde 2: 7
+Zijde 3: 2
+Deze zijden vormen geen driehoek.
+
+Zijde 1: 4
+Zijde 2: 5
+Zijde 3: 2
+Deze zijden vormen een driehoek.
+ */
---
+++ TestOefening5G/Test5G.cs
@@ -0,0 +1,17 @@
+﻿namespace TestOefening5G
+{
+    [TestClass]
+    public sealed class Test5G
+    {
+        [TestMethod]
+        public void TestMethod4_7_2()
+        {
+            TestHelper.HelperMethods.AssertOutputs("Zijde 1: Zijde 2: Zijde 3: Deze zijden vormen geen driehoek.", "4", "7", "2");
+        }
+        [TestMethod]
+        public void TestMethod4_5_2()
+        {
+            TestHelper.HelperMethods.AssertOutputs("Zijde 1: Zijde 2: Zijde 3: Deze zijden vormen een driehoek.", "4", "5", "2");
+        }
+    }
+}
