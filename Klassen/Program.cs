8767b6f6ac725bf955f629f33179e70bef39c69a
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-30 11:03:30 +0200

    data

---
+++ Klassen/Program.cs
@@ -0,0 +1,16 @@
+﻿string leerling1Naam = "Bert";
+int leerling1Leeftijd = 65;
+
+string leerling2Naam = "Ernie";
+int leerling2Leeftijd = 55;
+
+string leerling3Naam = "Bonnie";
+int leerling3Leeftijd = 29;
+
+string leerling4Naam = "Clyde";
+int leerling4Leeftijd = 30;
+
+Console.WriteLine($"{leerling1Naam}: {leerling1Leeftijd} jaar");
+Console.WriteLine($"{leerling2Naam}: {leerling2Leeftijd} jaar");
+Console.WriteLine($"{leerling3Naam}: {leerling3Leeftijd} jaar");
+Console.WriteLine($"{leerling4Naam}: {leerling4Leeftijd} jaar");
