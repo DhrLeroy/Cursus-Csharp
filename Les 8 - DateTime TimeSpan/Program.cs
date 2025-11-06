04d2249f0679a21294056967bb13a577534f2c6c
Renaud Leroy <remastered.king@gmail.com>
2025-11-06 09:46:32 +0100

    DT

---
+++ Les 8 - DateTime TimeSpan/Program.cs
@@ -0,0 +1,24 @@
+﻿DateTime geboortedatum = new DateTime(2009, 1, 18);
+
+if(geboortedatum.DayOfWeek == DayOfWeek.Sunday)
+{
+    Console.WriteLine("Ik werd geboren op een zondag");
+}
+
+DateTime schrikkeldatum = new DateTime(2023, 2, 28);
+
+if (DateTime.IsLeapYear(schrikkeldatum.Year))
+{
+    Console.WriteLine("Schrikkeljaar!");
+}
+
+var vandaag = DateTime.Now;
+
+var sindsGeboorte = vandaag - geboortedatum;
+
+var lesuur = new TimeSpan(0, 50, 0);
+
+var lesuur1 = DateTime.Today + new TimeSpan(8, 40, 0);
+var lesuur2 = lesuur1 + lesuur;
+
+Console.WriteLine(DateTime.Now.ToString("dddd, d MMMM yy HHumm"));
