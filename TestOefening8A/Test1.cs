9712e25683b08e04a12067825eecdfe61914983a
Renaud Leroy <remastered.king@gmail.com>
2025-11-06 10:16:22 +0100

    8A edit

---
+++ TestOefening8A/Test1.cs
@@ -0,0 +1,17 @@
+﻿namespace TestOefening8A
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void Test15januari2024()
+        {
+            TestHelper.HelperMethods.AssertOutputs("1 jaar", "15", "1", "2024");
+        }
+        [TestMethod]
+        public void Test15januari2024()
+        {
+            TestHelper.HelperMethods.AssertOutputs("1 jaar", "15", "1", "2024");
+        }
+    }
+}
