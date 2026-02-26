67c2f818f6dfd8f252d11a444f4840fabe33e594
RenaudLeroy <RenaudLeroy@WINDOWS-IMF6MMQ>
2026-02-26 09:27:03 +0100

    exception les

---
+++ Les9Exceptions/ABC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les9Exceptions
+{
+    public class A
+	{
+        public void Do() {
+            //try
+            //{
+                new B().Do();
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Er is iets foutgelopen in klasse A.");
+            //}
+            
+        }
+ 	}
+    public class B
+    {
+         public void Do() => new C().Do();
+    }
+    public class C
+    {
+        public int Do() => 1 / Int32.Parse("0");
+    }
+
+}
---
+++ Les9Exceptions/Program.cs
@@ -0,0 +1,52 @@
+﻿/*Console.Write("Geboortedatum: ");
+
+var geboortedatum = DateTime.Parse(null);
+
+if (2026 - geboortedatum.Year >= 18)
+    Console.WriteLine("Meerderjarig");
+else
+    Console.WriteLine("Minderjarig");*/
+/*
+while (true)
+{
+    try
+    {
+        Console.Write("Teller: ");
+        var teller = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Noemer: ");
+        var noemer = Convert.ToInt32(Console.ReadLine());
+
+        var quotient = teller / noemer;
+
+        Console.WriteLine(quotient);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Je moet de getallen in cijfers opgeven.");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("Het getal dat je opgaf was te groot of te klein.");
+    }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("Je kan niet delen door 0.");
+    }
+}
+
+
+*/
+
+using Les9Exceptions;
+
+try
+{
+    var a = new A();
+    a.Do();
+}
+catch
+{
+    Console.WriteLine("Er is ergens iets foutgelopen.");
+}
+
