26194bc829633de1e05922631f17b60beff47665
RenaudLeroy <RenaudLeroy@WINDOWS-AD559RD>
2026-03-03 09:20:53 +0100

    filmapp done

--- FilmApp/FilmPagina.cs
+++ FilmApp/FilmPagina.cs
@@ -1,11 +1,24 @@
+using FilmApp_databank;
+using FilmApp_klassen;
+
 namespace FilmApp;
 
 public class FilmPagina : ContentPage
 {
 	public FilmPagina()
-	{
-		var vStack = new VerticalStackLayout();
+    {
+        var db = new Databank();
+        var vStack = new VerticalStackLayout();
         Content = vStack;//NIET VERGETEN
+
+        var vStackFilms = new VerticalStackLayout();
+        vStack.Add(vStackFilms);//NIET VERGETEN
+        foreach (var film in db.Films)
+        {
+            var lblFilm = new Label();
+            lblFilm.Text = $"{film.Titel} ({film.Duurtijd_minuten} minuten)";
+            vStackFilms.Add(lblFilm);
+        }
 
         var hStackTitel = new HorizontalStackLayout();
 		var lblTitel = new Label();
@@ -20,7 +33,7 @@ public class FilmPagina : ContentPage
         var lblDuurtijd = new Label();
         lblDuurtijd.Text = "Duurtijd (in minuten): ";
         var entryDuurtijd = new Entry();
-        entryTitel.Placeholder = "Duurtijd (in minuten)";
+        entryDuurtijd.Placeholder = "Duurtijd (in minuten)";
         hStackDuurtijd.Add(lblDuurtijd); //NIET VERGETEN
         hStackDuurtijd.Add(entryDuurtijd);//NIET VERGETEN
         vStack.Add(hStackDuurtijd);//NIET VERGETEN
@@ -33,6 +46,29 @@ public class FilmPagina : ContentPage
         hStackLiveAction.Add(lblLiveAction); //NIET VERGETEN
         hStackLiveAction.Add(chckLiveAction);//NIET VERGETEN
         vStack.Add(hStackLiveAction);//NIET VERGETEN
+
+        var btnOpslaan = new Button();
+        vStack.Add(btnOpslaan);//NIET VERGETEN
+        btnOpslaan.Text = "Opslaan";
+        btnOpslaan.Clicked += (s, e) =>
+        {
+            var nieuweFilm = new Film();
+            nieuweFilm.Titel = entryTitel.Text;
+            nieuweFilm.Duurtijd_minuten = Convert.ToInt32(entryDuurtijd.Text);
+            nieuweFilm.LiveAction = chckLiveAction.IsChecked;
+            db.Films.Add(nieuweFilm);
+            db.SaveChanges();
+            entryTitel.Text = "";
+            entryDuurtijd.Text = "";
+            chckLiveAction.IsChecked = true;
+            vStackFilms.Clear(); 
+            foreach (var film in db.Films)
+            {
+                var lblFilm = new Label();
+                lblFilm.Text = $"{film.Titel} ({film.Duurtijd_minuten} minuten)";
+                vStackFilms.Add(lblFilm);
+            }
+        };
 
     }
 }
