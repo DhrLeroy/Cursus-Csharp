174fb55c69a01b741431f94f687911924034d2b0
RenaudLeroy <RenaudLeroy@WINDOWS-AD559RD>
2026-03-04 12:07:29 +0100

    MAUI videogames

---
+++ VideogamesApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "VideogamesApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "VideogamesApp.Pages")]
