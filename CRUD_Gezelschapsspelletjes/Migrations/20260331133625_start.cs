a06cf679fa3a8a993ef8f7fcd8ff52c9e29668b3
RenaudLeroy <RenaudLeroy@WINDOWS-AD559RD>
2026-03-31 15:41:18 +0200

    crud config

---
+++ CRUD_Gezelschapsspelletjes/Migrations/20260331133625_start.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUD_Gezelschapsspelletjes.Migrations
+{
+    /// <inheritdoc />
+    public partial class start : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Gezelschapsspelletjes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Titel = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Beschrijving = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Minimum_Spelers = table.Column<int>(type: "int", nullable: false),
+                    Maximum_Spelers = table.Column<int>(type: "int", nullable: false),
+                    WerktOpBatterijen = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gezelschapsspelletjes", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Gezelschapsspelletjes");
+        }
+    }
+}
