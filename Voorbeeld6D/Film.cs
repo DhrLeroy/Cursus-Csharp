da016eb0988739724e0866a27c8c138ed3364f1d
RenaudLeroy <RenaudLeroy@WINDOWS-AD559RD>
2026-04-21 10:34:52 +0200

    LINQ voorbeeld

---
+++ Voorbeeld6D/Film.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Films.Classes
+{
+    public class Film
+    {
+        public Reeks Reeks { get; set; }
+        public string Regisseur { get; set; }
+        public string Naam { get; set; }
+        public int Releasejaar { get; set; }
+        public List<Rol> Rollen { get; set; }
+
+        public Film(Reeks reeks, string naam, string regisseur, int release, params Rol[] rollen)
+        {
+            Reeks = reeks;
+            Naam = naam;
+            Regisseur = regisseur;
+            Releasejaar = release;
+            Rollen = rollen.ToList();
+        }
+
+        public override string ToString() => $"{(string.IsNullOrEmpty(Reeks?.Naam) ? "" : $"{Reeks.Naam}: ")}{Naam} ({Releasejaar})";
+    }
+}
---
+++ Voorbeeld6D/Rol.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Films.Classes
+{
+    public class Rol
+    {
+        public List<Film> Films { get; set; } // A role exists in multiple films
+        public string Naam { get; set; }
+        public List<Acteur> Acteurs { get; set; }
+
+        public Rol(string naam, params Acteur[] acteurs)
+        {
+            Films = new List<Film>();
+            Naam = naam;
+            Acteurs = acteurs.ToList();
+            foreach (var acteur in Acteurs)
+            {
+                acteur.Rollen.Add(this);
+            }
+        }
+
+        public void AddFilm(Film film)
+        {
+            Films.Add(film);
+        }
+    }
+}
